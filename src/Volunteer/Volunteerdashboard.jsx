50ce072503ca6e15f2ef8dde7bc68943154b40e3
Adityakumarma <adityakumarma@gmail.com>
2026-02-18 17:30:21 +0530

    update

--- src/Volunteer/Volunteerdashboard.jsx
+++ src/Volunteer/Volunteerdashboard.jsx
@@ -197,7 +197,7 @@ function Volunteerdashboard() {
           {availabledonation &&
             <div className='grid grid-cols-1 md:grid-cols-3 gap-8 mt-12'>
               {available.map(item => (
-                <div className="border rounded-xl overflow-hidden shadow-md bg-white w-200 mt-4">
+                <div className="border rounded-xl overflow-hidden shadow-md bg-white ">
                   <img
                     src={`${serverURL}/imguploads/${item.uploadImages[0]}`}
                     alt=""
