06a48fb95abfafe5d49fb4bd69f8a96dfb6888fc
Adityakumarma <adityakumarma@gmail.com>
2026-02-18 17:34:57 +0530

    update

--- src/Admin/AdminHome.jsx
+++ src/Admin/AdminHome.jsx
@@ -288,7 +288,8 @@ const donorsCount = userslistDetails.filter(item => item.role == "donor").length
             </table>
           </div>}
 
-        {alldonationsbutton && <div className='border p-7 mt-8 rounded-2xl w-350 m-10 shadow-2xl ms-20'>
+        {alldonationsbutton && 
+        <div className='border p-7 mt-8 rounded-2xl w-350 m-10 shadow-2xl ms-12'>
           <h1 className='text-3xl'>All Donations</h1>
           <p className='text-gray-600 mb-14'>Manage all donation activity</p>
           <table >
